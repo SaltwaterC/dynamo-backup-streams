8496c3b97126a951b40130c2ddd07bb137c02c44
SaltwaterC <saltwaterc@gmail.com>
2017-09-08 20:37:18 +0100

    Properly measure the capacity requirements for auto scaling.

--- test/backup.js
+++ test/backup.js
@@ -9,7 +9,8 @@ var table = process.argv[2];
 
 var bs = new Backup({
   table: table,
-  capacityPercentage: 100
+  capacityPercentage: 100,
+  delay: 900
 });
 var gzip = zlib.createGzip({
   level: 9
@@ -19,20 +20,20 @@ var ws = fs.createWriteStream(table + '.json.gz');
 bs.pipe(gzip).pipe(ws);
 
 // outputs a lot less noise compared to listening for 'progress' events
-var progress = setInterval(function () {
+var progress = setInterval(function() {
   var percent = 0;
-  if(bs.itemsCount > 0 && bs.itemsProcessed > 0) {
+  if (bs.itemsCount > 0 && bs.itemsProcessed > 0) {
     percent = bs.itemsProcessed / bs.itemsCount * 100;
     if (percent > 100) {
       percent = 100;
     }
 
-    if (percent == 100) {
+    if (percent === 100) {
       clearInterval(progress);
     }
   }
 
-  console.log(table + ' progress: %f%', percent);
+  console.log(table + ' progress: %f% - %d of %d; capacity %d - used %d', percent, bs.itemsProcessed, bs.itemsCount, bs.limit, bs.units);
 }, 1000);
 
 bs.on('end', function() {
